91c7c6fb26b30fa5c930b83b30008925cd9a6189
Nadiia <zoryvchaknv@gmail.com>
2025-02-24 21:08:13 +0200

    solution for movies form add

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
+import { useState } from 'react';
 
 export const App = () => {
+  const [movie, setMovie] = useState<Movie[]>(moviesFromServer);
+
+  const onAdd = (currMovie: Movie) => {
+    setMovie(prevMovie => [...prevMovie, currMovie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movie} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={onAdd} />
       </div>
     </div>
   );
